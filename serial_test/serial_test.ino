fb9c1e0214e83a0c761058b440d4a0d97814a2b2
FarOutWest <westDcastro@gmail.com>
2017-04-24 16:20:32 -0600

    updated with amp readings

--- serial_test/serial_test.ino
+++ serial_test/serial_test.ino
@@ -1,13 +1,17 @@
-char dataString[50] = {0};
-int a =0; 
-
+int inputVoltPin = A0;
+int inputAmpPin = A1;
+float resistance = 1500;
+ 
 void setup() {
-  Serial.begin(9600);              //Starting serial communication
+  Serial.begin(9600);              
 }
   
 void loop() {
-  a++;                          // a value increase every loop
-  sprintf(dataString,"%02X",a); // convert a value to hexa 
-  Serial.println(dataString);   // send the data
-  delay(1000);                  // give the loop some break
+  int voltValue = analogRead(inputVoltPin);
+  int ampValue = analogRead(inputAmpPin);    
+  float voltage = voltValue * (5.0 / 1023.0);  
+  float amperage = voltage / resistance;              
+  Serial.println(voltage);  
+  Serial.println(amperage); 
+  delay(1000);                
 }
