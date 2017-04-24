5246c4ffbf5138fdbd1a26119f4d28998a57d164
FarOutWest <westDcastro@gmail.com>
2017-04-24 16:16:54 -0600

    updated with amperage readings

--- serial_test.py
+++ serial_test.py
@@ -1,8 +1,10 @@
 import serial
 
-ser = serial.Serial('/dev/ttyACM0',9600)
+ser = serial.Serial('/dev/ttyACM0', 9600)
 
 while True:
 	read_serial = ser.readline()
 	volts = str(float(ser.readline()))
+    amps = str(float(ser.readline()))
 	print ("Voltage:",volts)
+    print ("Amps:",amps)
