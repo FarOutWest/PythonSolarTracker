c9a4f7fe94c8ad212766e0768dce74858c0df9f3
marcopolobrewing <marcopolobrewing@gmail.com>
2017-04-24 15:29:11 -0600

    updated serial communications

--- serial_test.py
+++ serial_test.py
@@ -1,9 +1,9 @@
 import serial
 
 ser = serial.Serial('/dev/ttyACM0',9600)
-s = [0]
+
 while True:
-	read_serial=ser.readline()
-	s[0] = str(int (ser.readline(),16))
-	print s[0]
+	read_serial = ser.readline()
+	value = str(int (ser.readline(),16))
+	print value
 	print read_serial
