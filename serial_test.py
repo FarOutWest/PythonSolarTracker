d49e31c3d24a2c08f2026ac2259413bacea7a43e
FarOutWest <marcopolobrewing@gmail.com>
2017-04-24 15:51:40 -0600

    updating the voltage readings for serial communications

--- serial_test.py
+++ serial_test.py
@@ -4,5 +4,5 @@ ser = serial.Serial('/dev/ttyACM0',9600)
 
 while True:
 	read_serial = ser.readline()
-	value = str(float(ser.readline()))
-	print ("Voltage:",value)
+	VoltValue = str(float(ser.readline()))
+	print ("Voltage:",VoltValue)
