c538a8e946189ba095780add366f605e1de4a9d6
marcopolobrewing <marcopolobrewing@gmail.com>
2017-04-24 15:30:21 -0600

    fixed stupid mistake

--- serial_test.py
+++ serial_test.py
@@ -4,6 +4,6 @@ ser = serial.Serial('/dev/ttyACM0',9600)
 
 while True:
 	read_serial = ser.readline()
-	value = str(int (ser.readline(),16))
+	value = str(int (ser.readline()))
 	print value
 	print read_serial
