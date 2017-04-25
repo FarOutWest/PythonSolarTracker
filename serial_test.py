b26a433f45fc6ee7b56f3eb2fa0a542394549979
FarOutWest <westDcastro@gmail.com>
2017-04-24 18:28:14 -0600

    updated the serial connections and verified that the arduino code works

--- serial_test.py
+++ serial_test.py
@@ -3,8 +3,7 @@ import serial
 ser = serial.Serial('/dev/ttyACM0', 9600)
 
 while True:
-	read_serial = ser.readline()
 	volts = str(float(ser.readline()))
     amps = str(float(ser.readline()))
-	print ("Voltage:",volts)
-    print ("Amps:",amps)
+	print ("Voltage:", volts)
+    print ("Amps:", amps)
--- serial_test/serial_test.ino
+++ serial_test/serial_test.ino
@@ -1,17 +1,20 @@
-int inputVoltPin = A0;
-int inputAmpPin = A1;
-float resistance = 1500;
+float resistance = 1.5;
+float voltage = 0;
+float amperage = 0;
  
 void setup() {
   Serial.begin(9600);              
 }
   
 void loop() {
-  int voltValue = analogRead(inputVoltPin);
-  int ampValue = analogRead(inputAmpPin);    
-  float voltage = voltValue * (5.0 / 1023.0);  
-  float amperage = voltage / resistance;              
-  Serial.println(voltage);  
+  Serial.println(voltage); 
   Serial.println(amperage); 
+  
+  analogReference(DEFAULT);
+  float voltValue = analogRead(A0);
+  
+  voltage = voltValue * (5.0 / 1023.0);  
+  amperage = voltage / resistance; 
+  
   delay(1000);                
 }
