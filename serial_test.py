c8c97a81a561671360085e5596abb0ff7e347cbd
FarOutWest <westDcastro@gmail.com>
2017-04-25 14:46:49 -0600

    hopefully this will work for serial communications

--- serial_test.py
+++ serial_test.py
@@ -2,7 +2,13 @@ import Serial
 
 ser = serial.Serial('/dev/ttyACM0', 9600)
 volts = []
+#amps = []
+timepoints = []
 
 while True:
 	volts.append(float(ser.readline()))
+    #amps.append(float(ser.readline()))
+    timepoints.append(float(ser.readline()))
 	print ("Voltage:", volts)
+    #print ("Amperage:", amps)
+    print ("timepoints:", timepoints)
